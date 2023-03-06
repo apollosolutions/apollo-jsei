340fbaa701fcfa91b43c06ad8d03b5f0562273a9
Martin Bonnin <martin@mbonnin.net>
2023-03-06 23:14:53 +0100

    remove mavenLocal as a dependency

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,9 +1,8 @@
 listOf(pluginManagement.repositories, dependencyResolutionManagement.repositories).forEach {
     it.apply {
         mavenCentral()
-        mavenLocal()
-//        maven {
-//            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-//        }
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
 }
